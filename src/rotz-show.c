4a05b758690bf5946876bddc0b495c2584db4290
Sebastian Freundt <freundt@ga-group.nl>
2013-05-27 07:04:50 +0000

    fix, PEBKAC, actually use the getopt parser from rolf-show-clo.ggo
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/rotz-show.c
+++ src/rotz-show.c
@@ -96,8 +96,8 @@ rotz_sym(const char *sym)
 #if defined __INTEL_COMPILER
 # pragma warning (disable:593)
 #endif	/* __INTEL_COMPILER */
-#include "rotz-add-clo.h"
-#include "rotz-add-clo.c"
+#include "rotz-show-clo.h"
+#include "rotz-show-clo.c"
 #if defined __INTEL_COMPILER
 # pragma warning (default:593)
 #endif	/* __INTEL_COMPILER */
