ba5a86f335750acdceb5b9c949a4385d344e1cb8
Sebastian Freundt <freundt@ga-group.nl>
2013-05-30 08:46:05 +0000

    minor, resort to rotz_massage_name() in iter_cb() of rotz-cloud
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/rotz-cloud.c
+++ src/rotz-cloud.c
@@ -67,10 +67,9 @@ iter_cb(rtz_vtx_t vid, const char *vtx, void *clo)
 	if (memcmp(vtx, RTZ_SYMSPC, sizeof(RTZ_SYMSPC) - 1) == 0) {
 		/* that's a symbol, vtx would be a tag then */
 		return;
-	} else if (memcmp(vtx, RTZ_TAGSPC, sizeof(RTZ_TAGSPC) - 1) == 0) {
-		vtx += RTZ_PRE_Z;
 	}
 
+	vtx = rotz_massage_name(vtx);
 	if (cp->pre.d && memcmp(vtx, cp->pre.d, cp->pre.z)) {
 		/* not matching prefix */
 		return;
